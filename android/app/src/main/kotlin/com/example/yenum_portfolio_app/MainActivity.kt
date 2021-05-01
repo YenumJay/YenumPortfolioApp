ff6d11be842487d9a010ddb0096998d0a148f367
Chukwuyenum Adeshina <adeshina.jude@gmail.com>
2021-05-01 23:43:40 +0100

    The inputs made have been cleared stated on the code

---
+++ android/app/src/main/kotlin/com/example/yenum_portfolio_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.yenum_portfolio_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyPortfolio());
+}
+
+class MyPortfolio extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          foregroundDecoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("lib/img/foto.png"),
+            )
+          ),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("lib/img/AppBackground.png"),
+                fit: BoxFit.cover)),
+          child: SafeArea(
+            child: Column(
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.only(top: 300),
+                  child: Text(
+                    'CHUKWUYENUM JUDE ADESHINA',
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      fontSize: 17,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.only(top: 60),
+                  child: Text(
+                    'ABOUT ME:',
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      fontSize: 17,
+                      color: Colors.yellow,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.all(10),
+                  child: Text(
+                    'In a population of over 200 million people in Nigeria, I believe i have an opportunity to make a real difference through technology by providing smart and disruptive ways of solving the challenges we face daily. I am passionate, driven, goal oriented, a great team player and i have a high sense of responsibility to whatever i commitment myself to.',
+                    textAlign: TextAlign.justify,
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.white,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    height: 350,
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.all(0),
+                    child: Image(
+                      image: AssetImage("lib/img/icons.png",
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
